8034a8ffeebdbc4b1c5c7f03b1168af707ace9be
Filip Troníček <filip.tronicek@seznam.cz>
2022-09-22 20:34:44 +0200

    Split code and adopt changes from smaller PR

---
+++ scripts/lib/utils.js
@@ -0,0 +1,35 @@
+export const getPrParticipants = (pr) =>
+  pr.participants.nodes
+    .map(({ login }) => login)
+    .filter((login) => !["roboquat"].includes(login))
+    .sort()
+    .join(",");
+
+export const parseReleaseNote = (pr) => {
+  const releaseNoteMatch = pr.body.match(/```release-notes?\r?\n(.+?)\r?\n```/);
+  const releaseNoteText = releaseNoteMatch && releaseNoteMatch[1].trim();
+  if (!releaseNoteMatch || releaseNoteText.toUpperCase() === "NONE") return;
+  return releaseNoteText;
+};
+
+export const replaceContentOfBlock = (blockName, blockContent, fileContent) => {
+  const startingLine = `<!--- BEGIN_${blockName} -->`;
+  const endingLine = `<!--- END_${blockName} -->`;
+
+  const startingLineIndex = fileContent.indexOf(startingLine);
+  const endingLineIndex = fileContent.indexOf(endingLine);
+
+  if (startingLineIndex === -1 || endingLineIndex === -1) {
+    throw new Error(
+      `Could not find ${startingLine} and/or ${endingLine} in the changelog file`
+    );
+  }
+
+  const newContent = [
+    fileContent.slice(0, startingLineIndex + startingLine.length),
+    blockContent,
+    fileContent.slice(endingLineIndex),
+  ].join("\r\n");
+
+  return newContent;
+};
