a0de937b9b73801777ad379ecd4c82aeee2c30cf
Filip Troníček <filip@gitpod.io>
2022-10-11 16:52:53 +0000

    Extract argument parsing and octokit init

--- scripts/changelog/index.js
+++ scripts/changelog/index.js
@@ -1,43 +1,19 @@
-import { Octokit } from "octokit";
-import { paginateGraphql } from "@octokit/plugin-paginate-graphql";
-import { getFormattedMonthBoundaries } from "./lib/dates.js";
 import {
   generatePrChangelogLine,
   processRepository,
-  outputResults,
-  ensureGithubToken,
-  sayHello,
   sortByCategoryOrder,
   formatChangelogCategory,
-  helpMenu,
+  createOctokitClient,
 } from "./lib/utils.js";
+import { outputResults, parseArgs } from "./lib/cli.js";
 import { lineBreak, prCategories, repos } from "./lib/config.js";
 import minimist from "minimist";
 
 const argv = minimist(process.argv.slice(2));
 
-const OctokitWithPlugins = Octokit.plugin(paginateGraphql);
-
 export const main = async () => {
-  if (argv.help || argv.h) {
-    helpMenu();
-    process.exit(0);
-  }
-
-  const [firstBusinessDay, lastBusinessDay] = getFormattedMonthBoundaries();
-  const releaseDate = argv._[0] || lastBusinessDay;
-  const from = argv._[1] || firstBusinessDay;
-  const to = argv._[2] || lastBusinessDay;
-  const githubToken = ensureGithubToken();
-
-  if (!githubToken) {
-    process.exit(1);
-  }
-
-  const octokit = new OctokitWithPlugins({
-    auth: githubToken,
-  });
-  await sayHello(octokit);
+  const { releaseDate, from, to, githubToken } = parseArgs(argv);
+  const octokit = await createOctokitClient(githubToken);
 
   let categorizedPrs = prCategories;
   for (const repo of repos) {
@@ -52,12 +28,12 @@ export const main = async () => {
 
   for await (const [index, category] of categorizedPrs.entries()) {
     const prs = category.prs.map(generatePrChangelogLine).join("");
-    const formattedCategory = await formatChangelogCategory(
+    const formattedCategoryContent = await formatChangelogCategory(
       prs,
       category,
       releaseDate
     );
-    categorizedPrs[index] = { ...category, ...formattedCategory };
+    categorizedPrs[index] = { ...category, ...formattedCategoryContent };
   }
   const perCategoryPrContent = categorizedPrs
     .sort(sortByCategoryOrder)
